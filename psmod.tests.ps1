295079111ac6fd0b3e81576f90eb710830d5c321
Trevor Sullivan <trevor.sullivan@clusterhq.com>
2016-11-21 22:52:20 +0000

    Adding more Pester tests

--- psmod.tests.ps1
+++ psmod.tests.ps1
@@ -5,11 +5,18 @@ Write-Information -Message 'Invoking Pester tests'
 Import-Module -Name /psmod -Verbose
 
 describe 'psmod' {
+
 	it 'Should have an AddValues function' {
 		(Get-Command -Module psmod -Name AddValues).Count | Should be 1
 	}
 
-	it 'Should output 4 when parameters 2 and 2 are passed in' {
-		AddValues -First 2 -Second 2 | Should be 4
+  context 'AddValues Function' {
+    it 'Should output 4 when parameters 2 and 2 are passed in' {
+	  	AddValues -First 2 -Second 2 | Should be 4
+  	}
+  
+  	it 'Should throw an exception when a string is passed in' {
+			{ AddValues -First somestring -Second 2 } | Should throw
+    }
 	}
 }
